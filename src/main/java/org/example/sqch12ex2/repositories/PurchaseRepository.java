905e7416f82e5a28ebc75300fbddc5a8132b8dcf
sunwooc55 <sunwooc55@gmail.com>
2025-09-26 22:18:35 +0900

    1

--- src/main/java/org/example/sqch12ex2/repositories/PurchaseRepository.java
+++ src/main/java/org/example/sqch12ex2/repositories/PurchaseRepository.java
@@ -17,7 +17,7 @@ public class PurchaseRepository {
     }
 
     public void storePurchase(Purchase purchase){
-        String sql = "INSERT INTO purchase VALUES (NULL,?,?)";
+        String sql = "INSERT INTO purchase (product, price) VALUES (?,?)";
         jdbc.update(sql, purchase.getProduct(), purchase.getPrice());
     }
 
